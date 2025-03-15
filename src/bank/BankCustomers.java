c096c90918d898a2949df58699259eb612565061
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-15 03:57:11 +0330

    add customer class

--- src/bank/BankCustomers.java
+++ src/bank/BankCustomers.java
@@ -1,4 +1,31 @@
 package bank;
 
+import java.util.ArrayList;
+
 public class BankCustomers {
+    private static ArrayList<BankAccount> accountsList = new ArrayList<>();
+
+    public void addAccount(BankAccount account) {
+        accountsList.add(account);
+    }
+
+    public void showAllBalances() {
+        for (BankAccount account : accountsList) {
+            System.out.println("Account Holder Name: " + account.getAccountHolderName());
+            System.out.println("Account Number: " + account.getAccountNumber());
+            System.out.println("Balance: $" + account.getBalance());
+            System.out.println();
+        }
+    }
+
+    public BankAccount findAccount(String accountNumber) {
+        for (BankAccount account : accountsList) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        System.out.println("Account not found");
+        return null;
+    }
+
 }
